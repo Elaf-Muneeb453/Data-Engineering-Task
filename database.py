8bb4373d7c1689d0667202fe584e64f9fd00c405
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-13 16:43:37 +0500

    update

--- database.py
+++ database.py
@@ -1,13 +1,3 @@
-# import pandas as pd
-
-# import sqlite3
-
-# df = pd.read_csv("Data_Engineering_Challenge.csv")
-
-# conn = sqlite3.connect("csv_database.db")
-# df.to_sql("sales", conn, if_exists="replace", index=False)
-# conn.close()
-
 import pandas as pd
 from sqlalchemy import create_engine
 
