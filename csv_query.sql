1c8e39acca8d421666bcb9733adc33145985c9b9
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-20 20:06:57 +0500

    update

--- csv_query.sql
+++ csv_query.sql
@@ -8,26 +8,6 @@ CREATE TABLE sensor_data (
     total_voltage FLOAT
 );
 
-
--- SELECT
---     site_code,
---     date_trunc('hour', time_stamp) AS hourly_window,
---     source_tag
--- FROM sensor_data;
-
--- SELECT
---     site_code,
---     date_trunc('hour', time_stamp) AS hourly_window,
---     ROUND((COUNT(CASE WHEN TRIM(source_tag) LIKE '%Solar%' THEN 1 END)*3.0/60,3)) AS solar_count,
---     ROUND((COUNT(CASE WHEN TRIM(source_tag) LIKE '%Battery%' THEN 1 END)*3.0/60,3)) AS battery_count,
---     ROUND((COUNT(CASE WHEN TRIM(source_tag) LIKE '%DG%' THEN 1 END)*3.0/60,3)) AS dg_count
--- FROM sensor_data
--- GROUP BY
---     site_code,
---     date_trunc('hour', time_stamp)
--- ORDER BY
---     hourly_window;
-
 SELECT
     site_code,
     date_trunc('hour', time_stamp) AS hourly_window,
