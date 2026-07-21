56dd9a4ac415ca46280222c678cb74c496286312
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-21 14:41:34 +0500

    update

--- csv_query.sql
+++ csv_query.sql
@@ -33,3 +33,17 @@ GROUP BY
     date_trunc('hour', time_stamp)
 ORDER BY
     hourly_window;
+
+
+SELECT
+    site_code, 
+    date_trunc('hour', time_stamp) AS hourly_window,
+    ROUND(AVG(CASE WHEN TRIM(source_tag) LIKE '%DG%' THEN (total_load_current*total_voltage)/1000 END)::numeric,2) AS dg_kw,
+    ROUND(AVG(CASE WHEN TRIM(source_tag) LIKE '%Battery%' THEN (battery_total_current*total_voltage)/1000 END)::numeric,2) AS battery_kw,
+    ROUND(AVG(CASE WHEN TRIM(source_tag) LIKE '%Solar%' THEN (solar_output_current*total_voltage)/1000 END)::numeric,2) AS solar_kw
+FROM sensor_data
+GROUP BY
+    site_code,
+    date_trunc('hour', time_stamp)
+ORDER BY
+    hourly_window;
